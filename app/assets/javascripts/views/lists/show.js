f11712b0abbcf725e1c5f795a3b1e1b4e3978959
lily <lily@aa.io>
2015-06-05 18:21:17 -0400

    growform animation

--- app/assets/javascripts/views/lists/show.js
+++ app/assets/javascripts/views/lists/show.js
@@ -1,11 +1,13 @@
 Bellino.Views.ListShow = Backbone.View.extend({
   template: JST['lists/show'],
+  linkTemplate: JST['cards/new_link'],
 
   className: 'list-show group',
 
   events: {
     'click .new-card-link': 'newCard',
-    'click .list-delete': 'destroyList'
+    'click .list-delete': 'destroyList',
+    'click li': 'closeNewCard'
   },
 
   initialize: function (options) {
@@ -31,11 +33,20 @@ Bellino.Views.ListShow = Backbone.View.extend({
 
   newCard: function (event) {
     event.preventDefault();
+    this.$el.find('.new-card-form').removeClass("animation-shrinkform");
+    this.$el.find('.new-card-form').addClass("animation-growform");
     var newCardView = new Bellino.Views.CardNew({
       list: this.list,
       board: this.board
     });
     this.$el.find('.new-card-form').html(newCardView.render().$el);
+  },
+
+  closeNewCard: function (event) {
+    if (event.toElement.className != "list-item") { return; }
+    this.$el.find('.new-card-form').html(this.linkTemplate());
+    this.$el.find('.new-card-form').removeClass("animation-growform");
+    this.$el.find('.new-card-form').addClass("animation-shrinkform");
   },
 
   destroyList: function (event) {
